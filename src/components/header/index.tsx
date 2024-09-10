39b04bafb8675424ca7339c01cdf9ad103e3d217
Brooke Daniels <brooke.e.daniels@outlook.com>
2024-09-10 14:24:13 -0700

    Added initial reset button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,6 +6,9 @@ const Header = () => {
       <div className="logo">
         <h1>MyApp</h1>
       </div>
+      <div>
+        <button onClick={() => console.log('Reset clicked')}><h1>Reset</h1></button>
+      </div>
     </header>
   );
 };
