b05363dee886adf30f87c7bf27f8963ec6936c0b
Brooke Daniels <brooke.e.daniels@outlook.com>
2024-09-12 23:01:07 -0700

    Added story mode

--- src/components/card/deck.tsx
+++ src/components/card/deck.tsx
@@ -1,7 +1,40 @@
 import { CardBase } from ".";
 import { CardType } from "../grid";
 
-export const deck: CardBase[] = [
+export const roundOneDeck: CardBase[] = [
+  {
+    id: 0,
+    type: CardType.Apple,
+    matched: false,
+  },
+  {
+    id: 1,
+    type: CardType.Apple,
+    matched: false,
+  },
+  {
+    id: 2,
+    type: CardType.Banana,
+    matched: false,
+  },
+  {
+    id: 3,
+    type: CardType.Banana,
+    matched: false,
+  },
+  {
+    id: 4,
+    type: CardType.Grape,
+    matched: false,
+  },
+  {
+    id: 5,
+    type: CardType.Grape,
+    matched: false,
+  },
+];
+
+export const roundTwoDeck: CardBase[] = [
   {
     id: 0,
     type: CardType.Bear,
@@ -60,6 +93,69 @@ export const deck: CardBase[] = [
   {
     id: 11,
     type: CardType.Pig,
+    matched: false,
+  },
+];
+
+export const roundThreeDeck: CardBase[] = [
+  {
+    id: 0,
+    type: CardType.Cloud,
+    matched: false,
+  },
+  {
+    id: 1,
+    type: CardType.Cloud,
+    matched: false,
+  },
+  {
+    id: 2,
+    type: CardType.Comet,
+    matched: false,
+  },
+  {
+    id: 3,
+    type: CardType.Comet,
+    matched: false,
+  },
+  {
+    id: 4,
+    type: CardType.Moon,
+    matched: false,
+  },
+  {
+    id: 5,
+    type: CardType.Moon,
+    matched: false,
+  },
+  {
+    id: 6,
+    type: CardType.Planet,
+    matched: false,
+  },
+  {
+    id: 7,
+    type: CardType.Planet,
+    matched: false,
+  },
+  {
+    id: 8,
+    type: CardType.Satelite,
+    matched: false,
+  },
+  {
+    id: 9,
+    type: CardType.Satelite,
+    matched: false,
+  },
+  {
+    id: 10,
+    type: CardType.Star,
+    matched: false,
+  },
+  {
+    id: 11,
+    type: CardType.Star,
     matched: false,
   },
 ];
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,28 +1,55 @@
 import "./card.css";
 import { useMemo } from "react";
 import { CardType } from "../grid";
+import Apple from "../../assets/apple.svg";
+import Banana from "../../assets/banana.svg";
 import Bear from "../../assets/bear.svg";
 import Cat from "../../assets/cat.svg";
+import Cloud from "../../assets/cloud.svg";
+import Comet from "../../assets/comet.svg";
 import Cow from "../../assets/cow.svg";
 import Crab from "../../assets/crab.svg";
 import Duck from "../../assets/duck.svg";
+import Grape from "../../assets/grape.svg";
+import Moon from "../../assets/moon.svg";
 import Pig from "../../assets/pig.svg";
+import Planet from "../../assets/planet.svg";
+import Satelite from "../../assets/satelite.svg";
+import Star from "../../assets/star.svg";
 import CardBack from "../../assets/card-back.svg";
 
 function getImage(type: CardType) {
   switch (type) {
+    case CardType.Apple:
+      return Apple;
+    case CardType.Banana:
+      return Banana;
     case CardType.Bear:
       return Bear;
     case CardType.Cat:
       return Cat;
+    case CardType.Cloud:
+      return Cloud;
+    case CardType.Comet:
+      return Comet;
     case CardType.Cow:
       return Cow;
     case CardType.Crab:
       return Crab;
     case CardType.Duck:
       return Duck;
+    case CardType.Grape:
+      return Grape;
+    case CardType.Moon:
+      return Moon;
     case CardType.Pig:
       return Pig;
+    case CardType.Planet:
+      return Planet;
+    case CardType.Satelite:
+      return Satelite;
+    case CardType.Star:
+      return Star;
   }
 }
 
@@ -42,9 +69,10 @@ export interface CardProps {
 const Card = ({ item, disabled, flipped, onClick }: CardProps) => {
   const image = getImage(item.type);
 
+  // Set the css class based on if the card is flipped and/or matched
   const classes = useMemo(() => {
     const matched = " matched";
-    const backClass = "card back";
+    const backClass = "card";
     const frontClass = "card front";
     if (item.matched) {
       return frontClass + matched;
